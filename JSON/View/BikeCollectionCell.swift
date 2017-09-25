c84c5866693549d1aa8eda5210b9a91461e9e3fb
Matt Deuschle <mdeuschle@gmail.com>
2017-09-25 13:57:33 -0700

    set up vcs and files

---
+++ JSON/View/BikeCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  BikeCollectionCell.swift
+//  JSON
+//
+//  Created by Matt Deuschle on 9/25/17.
+//  Copyright © 2017 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class BikeCollectionCell: UICollectionViewCell {
+    
+}
---
+++ JSON/View/BikeTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  BikeTableCell.swift
+//  JSON
+//
+//  Created by Matt Deuschle on 9/25/17.
+//  Copyright © 2017 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class BikeTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
